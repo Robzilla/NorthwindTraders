f2f6e37f57d43b3798d8b729404359ae95a3c914
rhollamby <robert.hollamby@aemo.com.au>
2019-08-01 18:56:09 +1000

    Initial Migration to Azure Function

---
+++ Northwind.Api/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using Northwind.Persistence;
+using Northwind.Application.Interfaces;
+using Northwind.Common;
+using Northwind.Infrastructure;
+
+[assembly: FunctionsStartup(typeof(Northwind.Api.Startup))]
+
+namespace Northwind.Api
+{
+    class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+
+            // Add framework services.
+            builder.Services.AddTransient<INotificationService, NotificationService>();
+            builder.Services.AddTransient<IDateTime, MachineDateTime>();
+
+            string SqlConnection = Environment.GetEnvironmentVariable("SqlConnectionString");
+            builder.Services.AddDbContext<NorthwindDbContext>(
+                options => options.UseSqlServer(SqlConnection));
+        }
+    }
+}
