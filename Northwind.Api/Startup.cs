4b561e1b48b658b27562ae4201c1405bc0fbcea7
Robert Hollamby <robert@hollamby.net>
2019-08-02 22:56:49 +1000

    Wiring up services

--- Northwind.Api/Startup.cs
+++ Northwind.Api/Startup.cs
@@ -6,6 +6,12 @@ using Northwind.Persistence;
 using Northwind.Application.Interfaces;
 using Northwind.Common;
 using Northwind.Infrastructure;
+using AutoMapper;
+using System.Reflection;
+using Northwind.Application.Infrastructure.AutoMapper;
+using MediatR;
+using Northwind.Application.Products.Queries.GetProduct;
+using Northwind.Application.Infrastructure;
 
 [assembly: FunctionsStartup(typeof(Northwind.Api.Startup))]
 
@@ -15,14 +21,22 @@ namespace Northwind.Api
     {
         public override void Configure(IFunctionsHostBuilder builder)
         {
+            // Add AutoMapper
+            builder.Services.AddAutoMapper(new Assembly[] { typeof(AutoMapperProfile).GetTypeInfo().Assembly });
 
             // Add framework services.
             builder.Services.AddTransient<INotificationService, NotificationService>();
             builder.Services.AddTransient<IDateTime, MachineDateTime>();
 
+            // Add MediatR
+            builder.Services.AddMediatR(typeof(GetProductQueryHandler).GetTypeInfo().Assembly);
+            builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestPerformanceBehaviour<,>));
+            builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestValidationBehavior<,>));
+
+            // Add DbContext using SQL Server Provider
             string SqlConnection = Environment.GetEnvironmentVariable("SqlConnectionString");
-            builder.Services.AddDbContext<NorthwindDbContext>(
-                options => options.UseSqlServer(SqlConnection));
+            builder.Services.AddDbContext<INorthwindDbContext, NorthwindDbContext>(options =>
+                options.UseSqlServer(SqlConnection));
         }
     }
 }
